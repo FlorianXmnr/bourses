fada6a322657d21a182e306cd58d3bd6e271d67b
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-04 10:51:46 +0100

    modify connection.py

--- API/connection.py
+++ API/connection.py
@@ -5,9 +5,7 @@ password = "DJK7hi0GRSkl8upZ"
 host = "clusterbourse.60a6vhb.mongodb.net"
 dbname = "Stage"
 collection_name = "Bourse"
-symboles = ["MC.PA", "RMS.PA", "OR.PA", "CDI.PA", "TTE.PA", "AIR.PA", "SU.PA", "SAN.PA", "AI.PA", "EL.PA", "SAF.PA",
-            "CS.PA", "DG.PA", "BNP.PA", "DSY.PA", "KER.PA",
-            "BN.PA"]
+
 # URI de connexion
 uri = f"mongodb+srv://{user}:{password}@{host}/?retryWrites=true&w=majority&appName=ClusterBourse&ssl=true"
 # Se connecter à MongoDB
@@ -16,6 +14,9 @@ try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
     # Complétez cette liste avec les symboles réels
+    symboles = ["MC.PA", "RMS.PA", "OR.PA", "CDI.PA", "TTE.PA", "AIR.PA", "SU.PA", "SAN.PA", "AI.PA", "EL.PA", "SAF.PA",
+                "CS.PA", "DG.PA", "BNP.PA", "DSY.PA", "KER.PA",
+                "BN.PA"]
 except Exception as e:
     print(e)
 
