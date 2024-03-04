72c6e0d6b24e4f17bc484526174f813fc258c723
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-04 16:10:51 +0100

    modify update_data.py

--- API/update_data.py
+++ API/update_data.py
@@ -1,6 +1,20 @@
 import yfinance as yf
 from datetime import datetime, timedelta
-from connection import *
+import os
+from pymongo import MongoClient
+
+# from connection import *
+
+
+user = os.environ.get("MONGO_USER")
+password = os.environ.get("MONGO_PASSWORD")
+host = os.environ.get("MONGO_HOST")
+dbname = os.environ.get("MONGO_DBNAME")
+# Supposant que collection_name est utilisé ailleurs dans votre code.
+collection_name = os.environ.get("MONGO_COLLECTION_NAME")
+
+uri = f"mongodb+srv://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority&appName=ClusterBourse&tls=true"
+client = MongoClient(uri)
 
 # Sélectionner la base de données
 db = client[dbname]
