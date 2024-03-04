edf7ea0931a61255e6e4acb7e3f253da8d29fdce
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-04 10:14:17 +0100

    modify connection.py

--- API/connection.py
+++ API/connection.py
@@ -5,7 +5,9 @@ password = "DJK7hi0GRSkl8upZ"
 host = "clusterbourse.60a6vhb.mongodb.net"
 dbname = "Stage"
 collection_name = "Bourse"
-
+symboles = ["MC.PA", "RMS.PA", "OR.PA", "CDI.PA", "TTE.PA", "AIR.PA", "SU.PA", "SAN.PA", "AI.PA", "EL.PA", "SAF.PA",
+            "CS.PA", "DG.PA", "BNP.PA", "DSY.PA", "KER.PA",
+            "BN.PA"]
 # URI de connexion
 uri = f"mongodb+srv://{user}:{password}@{host}/?retryWrites=true&w=majority&appName=ClusterBourse"
 # Se connecter à MongoDB
@@ -13,9 +15,7 @@ client = MongoClient(uri)
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
-    symboles = ["MC.PA", "RMS.PA", "OR.PA", "CDI.PA", "TTE.PA", "AIR.PA", "SU.PA", "SAN.PA", "AI.PA", "EL.PA", "SAF.PA",
-                "CS.PA", "DG.PA", "BNP.PA", "DSY.PA", "KER.PA",
-                "BN.PA"]  # Complétez cette liste avec les symboles réels
+    # Complétez cette liste avec les symboles réels
 except Exception as e:
     print(e)
 
