eaec8a5aa3378a1dc5e4778b97bf165dbc0558b3
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-04 15:29:46 +0100

    modify

--- API/connection.py
+++ API/connection.py
@@ -1,16 +1,31 @@
 from pymongo import MongoClient
-
-
 import os
+"""user = "alexiszueraspro"
+password = "DJK7hi0GRSkl8upZ"
+host = "clusterbourse.60a6vhb.mongodb.net"
+dbname = "Stage"
+collection_name = "Bourse"""
 
-user = os.environ.get("MONGO_USER")
-password = os.environ.get("MONGO_PASSWORD")
-host = os.environ.get("MONGO_HOST")
-dbname = os.environ.get("MONGO_DBNAME")
-collection_name = os.environ.get("MONGO_COLLECTION_NAME")
+user = os.environ.get("Secrets.MONGO_USER")
+password = os.environ.get("Secrets.MONGO_PASSWORD")
+host = os.environ.get("Secrets.MONGO_HOST")
+dbname = os.environ.get("Secrets.MONGO_DBNAME")
+collection_name = os.environ.get("Secrets.MONGO_COLLECTION_NAME")
 
-uri = f"mongodb://{user}:{password}@{host}/?retryWrites=true&w=majority&appName=ClusterBourse&tlsInsecure=true"
+print(user)
+print(password)
+print(host)
+print(dbname)
+print(collection_name)
+
+uri = f"mongodb+srv://{user}:{password}@{host}/?retryWrites=true&w=majority&appName=ClusterBourse&tls=true"
 client = MongoClient(uri)
+
+
+"""# URI de connexion
+uri = f"mongodb+srv://{user}:{password}@{host}/?retryWrites=true&w=majority&appName=ClusterBourse"
+# Se connecter à MongoDB
+client = MongoClient(uri)"""
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
