069551224f19067e37a639eb8b3a2297953c0d2d
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-05 11:15:58 +0100

    modify main.yml & connection.py

--- API/connection.py
+++ API/connection.py
@@ -13,13 +13,13 @@ uri = f"mongodb+srv://{user}:{password}@{host}/?retryWrites=true&w=majority&appN
 """
 try:
 
-    user = os.environ("MONGO_USER")
-    password = os.environ("MONGO_PASSWORD")
-    host = os.environ("MONGO_HOST")
-    dbname = os.environ("MONGO_DBNAME")
-    collection_name = os.environ("MONGO_COLLECTION_NAME")
-
-    uri = f"mongodb+srv://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority&appName=ClusterBourse&tls=true"
+    user = os.environ.get("MONGO_USER")
+    password = os.environ.get("MONGO_PASSWORD")
+    host = os.environ.get("MONGO_HOST")
+    dbname = os.environ.get("MONGO_DBNAME")
+    collection_name = os.environ.get("MONGO_COLLECTION_NAME")
+    app_name = os.environ.get("MONGO_APP_NAME")
+    uri = f"mongodb+srv://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority&appName{app_name}=&tls=true"
 
     print(uri)
     client = MongoClient(uri)
