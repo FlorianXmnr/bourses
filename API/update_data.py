36d8fba3642e8f93e060681b8d482fd0ede34856
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-05 08:37:40 +0100

    modify update_data.py & new .env

--- API/update_data.py
+++ API/update_data.py
@@ -4,17 +4,21 @@ import os
 from pymongo import MongoClient
 
 # from connection import *
+from dotenv import load_dotenv
 
+
+load_dotenv()  # Prend les variables d'environnement depuis le fichier .env
 
 user = os.environ.get("MONGO_USER")
 password = os.environ.get("MONGO_PASSWORD")
 host = os.environ.get("MONGO_HOST")
 dbname = os.environ.get("MONGO_DBNAME")
-# Supposant que collection_name est utilisé ailleurs dans votre code.
 collection_name = os.environ.get("MONGO_COLLECTION_NAME")
-print("MONGO_USER:", os.environ.get("MONGO_USER"))
 
-uri = f"mongodb+srv://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority&appName=ClusterBourse"
+
+uri = f"mongodb+srv://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority&appName=ClusterBourse&tls=true"
+
+print(uri)
 
 client = MongoClient(uri)
 
