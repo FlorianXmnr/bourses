2f2c4196e2df59a9d75c5f9d5c226d0a39271fb5
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-05 17:06:39 +0100

    modify update_data.py

--- API/update_data.py
+++ API/update_data.py
@@ -2,21 +2,19 @@ import yfinance as yf
 from datetime import datetime, timedelta
 import os
 from pymongo import MongoClient
-from dotenv import load_dotenv
 
-load_dotenv()
 # from connection import *
 
-user = os.getenv("MONGO_USER")
-password = os.getenv("MONGO_PASSWORD")
-host = os.getenv("MONGO_HOST")
-dbname = os.getenv("MONGO_DBNAME")
-collection_name = os.getenv("MONGO_COLLECTION_NAME")
-
-
-uri = f"mongodb+srv://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority&appName=ClusterBourse&tls=true"
+user = os.environ.get("MONGO_USER")
+password = os.environ.get("MONGO_PASSWORD")
+host = os.environ.get("MONGO_HOST")
+dbname = os.environ.get("MONGO_DBNAME")
+collection_name = os.environ.get("MONGO_COLLECTION_NAME")
+app_name = os.environ.get("MONGO_APP_NAME")
+uri = f"mongodb+srv://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority&appName={app_name}&tls=true"
 
 print(uri)
+client = MongoClient(uri)
 """
 client = MongoClient(uri)
 
