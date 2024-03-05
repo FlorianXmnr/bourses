642101fb1180eb0f986afbbda6493033e7091082
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-05 10:14:39 +0100

    modify update_data.py & read_data.py

--- API/read_data.py
+++ API/read_data.py
@@ -1,14 +1,5 @@
 from connection import *
 from datetime import datetime, timedelta
-
-symboles = ["MC.PA", "RMS.PA", "OR.PA", "CDI.PA", "TTE.PA", "AIR.PA", "SU.PA", "SAN.PA", "AI.PA", "EL.PA", "SAF.PA",
-            "CS.PA", "DG.PA", "BNP.PA", "DSY.PA", "KER.PA","BN.PA"]
-# Sélectionner la base de données
-db = client[dbname]
-
-# Sélectionner la collection dans laquelle vous souhaitez insérer les données
-collection = db[collection_name]
-print(collection)
 # Calculer la date de la veille
 today = datetime.now()
 if today.weekday() == 0:
@@ -20,7 +11,7 @@ hier = hier.strftime('%d/%m/%Y')  # Formattez selon le format de date dans votre
 
 for symbole in symboles:
     # Trouver le document pour le symbole à la date d'hier
-    document = collection.find_one({"Symbole": symbole, "Date": hier})
+    document = collection_name.find_one({"Symbole": symbole, "Date": hier})
     if document:
         print(f"Dernière donnée pour {symbole} à la date d'hier : {document}")
     else:
--- API/update_data.py
+++ API/update_data.py
@@ -3,6 +3,17 @@ from datetime import datetime, timedelta
 from connection import *
 
 
+user = os.environ.get("MONGO_USER")
+password = os.environ.get("MONGO_PASSWORD")
+host = os.environ.get("MONGO_HOST")
+dbname = os.environ.get("MONGO_DBNAME")
+collection_name = os.environ.get("MONGO_COLLECTION_NAME")
+
+uri = f"mongodb+srv://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority&appName=ClusterBourse&tls=true"
+
+print(uri)
+
+
 client = MongoClient(uri)
 
 # Sélectionner la base de données
