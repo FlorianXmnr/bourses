15808e71e39f49974c949c5df587b789e28c2f05
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-05 14:03:20 +0100

    modify .env

--- API/update_data.py
+++ API/update_data.py
@@ -8,10 +8,10 @@ load_dotenv()
 # from connection import *
 
 user = os.getenv("MONGO_USER")
-password = os.environ.get("MONGO_PASSWORD")
-host = os.environ.get("MONGO_HOST")
-dbname = os.environ.get("MONGO_DBNAME")
-collection_name = os.environ.get("MONGO_COLLECTION_NAME")
+password = os.getenv("MONGO_PASSWORD")
+host = os.getenv("MONGO_HOST")
+dbname = os.getenv("MONGO_DBNAME")
+collection_name = os.getenv("MONGO_COLLECTION_NAME")
 
 
 uri = f"mongodb+srv://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority&appName=ClusterBourse&tls=true"
