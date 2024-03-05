90ffb19d4e8ce67367165fbff1688a1f6ae22f2a
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-05 09:51:30 +0100

    modify connection.py

--- API/connection.py
+++ API/connection.py
@@ -1,16 +1,8 @@
 from pymongo import MongoClient
 import os
 
-"""user = "alexiszueraspro"
-password = "DJK7hi0GRSkl8upZ"
-host = "clusterbourse.60a6vhb.mongodb.net"
-dbname = "Stage"
-collection_name = "Bourse"""
-
-"""# URI de connexion
-uri = f"mongodb+srv://{user}:{password}@{host}/?retryWrites=true&w=majority&appName=ClusterBourse"
 # Se connecter à MongoDB
-"""
+
 try:
 
     user = os.environ.get("MONGO_USER")
