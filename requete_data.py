2a8df29b4cd359e6315c595a0069c8290d2a0188
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-07 14:34:07 +0100

    how to know description

--- requete_data.py
+++ requete_data.py
@@ -1,16 +1,7 @@
 import os
-from pymongo import MongoClient
+from API.connection import *
 import pandas as pd
 
-# Retrieve connection info from environment variables
-user = os.environ['MONGO_USER']
-password = os.environ['MONGO_PASSWORD']
-host = os.environ['MONGO_HOST']
-dbname = os.environ['MONGO_DBNAME']
-collection_name = os.environ['MONGO_COLLECTION_NAME']
-
-uri = f"mongodb+srv://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority"
-client = MongoClient(uri)
 db = client[dbname]
 collection = db[collection_name]
 
