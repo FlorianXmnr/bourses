e89cfa50ca664775c29c184a4e5a44567292c7ec
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-07 14:54:15 +0100

    new request, maybe can see a plot ?

--- requete_data.py
+++ requete_data.py
@@ -1,6 +1,8 @@
 import os
 from API.connection import *
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 db = client[dbname]
 collection = db[collection_name]
@@ -22,3 +24,14 @@ selected_columns = df.loc[:, "Open":"Volume"]
 description = selected_columns.describe()
 
 print(description)
+
+
+
+
+# Assuming 'data' is a pandas DataFrame or similar
+plt.figure(figsize=(10, 6))
+plt.plot(df['Open'])
+plt.title('Open Price Over Time')
+plt.xlabel('Date')
+plt.ylabel('Open Price')
+plt.savefig('open_price_plot.png')
