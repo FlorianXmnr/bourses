5f908157aa47507edfc70ecde04ba31e7a2d8ecc
Alex3132 <alexis.zueras.pro@gmail.com>
2024-03-07 14:27:25 +0100

    how to know columns types

--- requete_data.py
+++ requete_data.py
@@ -22,16 +22,5 @@ documents = list(collection.find({"Symbole": symbol}))
 
 # Convert to DataFrame for analysis
 df = pd.DataFrame(documents)
-
-# Drop the MongoDB ID field
-df.drop(columns=['_id'], inplace=True)
-
-# Example analysis (replace with your own analysis)
-print(df.describe())
-
-cols_num = df.select_dtypes(include=['float64', 'int64']).columns
-
-# Calculer les statistiques seulement sur les colonnes numériques
-print("Médiane:\n", df[cols_num].median())
-print("Moyenne:\n", df[cols_num].mean())
-print("Percentiles:\n", df[cols_num].quantile([0.25, 0.5, 0.75]))
+# Supposons que df est votre DataFrame
+print(df.dtypes)
