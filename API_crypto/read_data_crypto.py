5389c074819ca9c3f6f5daeaa87dbe46d9fe5aee
Alexis Zueras <az.analytics.pro@gmail.com>
2024-03-19 11:17:34 +0100

    test read_data_crypto.py

--- API_crypto/read_data_crypto.py
+++ API_crypto/read_data_crypto.py
@@ -1,5 +1,7 @@
 from connection_crypto import *
 import csv
+import pandas as pd
+from datetime import *
 
 # MongoDB connection setup
 client = MongoClient(uri)
@@ -12,7 +14,7 @@ csv_directory = "data"
 os.makedirs(csv_directory, exist_ok=True)
 
 csv_file_path = os.path.join(csv_directory, "historical_data_crypto.csv")
-csv_columns = ['_id', 'Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Symbole']
+csv_columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Symbole']
 # Proceed with writing to CSV
 with open(csv_file_path, mode='w', newline='', encoding='utf-8') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=csv_columns, delimiter=";")
@@ -21,8 +23,14 @@ with open(csv_file_path, mode='w', newline='', encoding='utf-8') as csvfile:
     for symbol in symboles:
         documents = collection.find({"Symbole": symbol}).sort([("Symbole", 1), ("Date", 1)])
         for document in documents:
-            document.pop('_id', None)
+            document.pop('_id', None)  # Remove the '_id' field
+            # Convert the 'Date' field to datetime and format it
+            document['Date'] = datetime.strptime(document['Date'], '%d/%m/%Y')
             writer.writerow(document)
 
+# Sort the CSV file by 'Symbole' and 'Date'
+df = pd.read_csv(csv_file_path, delimiter=";")
+df.sort_values(['Symbole', 'Date'], inplace=True)
+df.to_csv(csv_file_path, index=False, sep=";")
 
 print(f"Data successfully written to {csv_file_path}")
--- requete_data.py
+++ requete_data.py
@@ -4,23 +4,10 @@ import matplotlib.pyplot as plt
 
 # Charger les données
 df = pd.read_csv('data/historical_data.csv', sep=";")
+df_crypto = pd
 df = df[df['Symbole'] == 'CS.PA']
 # Supposons que df est votre DataFrame
 df['Date'] = pd.to_datetime(df['Date'])
-
-############
-"""plt.figure(figsize=(10, 6))
-plt.plot(df['Date'], df['Open'])
-plt.title('Open Price Over Time')
-plt.xlabel('Date')
-plt.ylabel('Open Price')
-plt.show()"""
-
-
-#############
-df = df[df["Open"] >= 20]
-lignes = df.shape[0]
-print(f"Il y a {lignes} jours")
 
 plt.figure(figsize=(10, 6))
 plt.plot(df['Date'], df['Open'])
