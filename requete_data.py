e85e597548d6ca955109a5a1a28005585a9f2fdc
Alexis Zueras <az.analytics.pro@gmail.com>
2024-03-19 11:08:33 +0100

    test read_data.py

--- requete_data.py
+++ requete_data.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 # Charger les données
-df = pd.read_csv('data/Data.csv', sep=";")
+df = pd.read_csv('data/historical_data.csv', sep=";")
 df = df[df['Symbole'] == 'CS.PA']
 # Supposons que df est votre DataFrame
 df['Date'] = pd.to_datetime(df['Date'])
