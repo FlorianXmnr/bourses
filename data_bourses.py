5eed4d5f181cb6d0e78cb4e1f8b86df0d7e6e1a1
Alexis Zueras <144041917+AzAnalytics@users.noreply.github.com>
2024-03-25 14:10:53 +0100

    changes files

--- data_bourses.py
+++ data_bourses.py
@@ -1,12 +1,8 @@
 import streamlit as st
 import pandas as pd
-
 from sklearn.linear_model import LinearRegression
-
 import numpy as np
-from pandas.tseries.offsets import BDay
 
-from datetime import datetime, timedelta
 
 st.title("Mon Application Streamlit")
 
@@ -56,82 +52,107 @@ elif tab == "Cryptomonnaies":
     # Créer le graphique
     st.line_chart(filtered_df[bourses])
 elif tab == "Prédictions":
-    # Créer un DataFrame vide pour les prédictions
-    df_predictions = pd.DataFrame(
-        columns=['Symbole', 'Dernière Close', 'Date ATH', 'ATH', 'Période ATH', 'Tendance', 'Prediction J+30',
-                 'Evolution attendue'])
+    # Charger le fichier CSV dans un DataFrame
+    df["Date"] = pd.to_datetime(df["Date"])
 
-    # Pour chaque symbole d'action
-    for symbol in df['Symbole'].unique():
-        # Filtrer le DataFrame pour le symbole actuel
-        df_symbol = df[df['Symbole'] == symbol]
+    # Trier le DataFrame par la colonne de dates
+    df_tri = df.sort_values(by=['Symbole', 'Date'])
+    # Reformater la colonne 'Date' au format jour/mois/année
 
-        # Convertir la colonne de date en datetime
-        df_symbol['Date'] = pd.to_datetime(df_symbol['Date'])
-        start_date = pd.to_datetime('2024-02-05')
-        df_symbol = df_symbol[df_symbol['Date'] >= start_date]
+    # Sélectionner la dernière valeur de 'Close' pour chaque symbole
+    last_close_df = df_tri.groupby('Symbole')['Close'].last().reset_index()
+    # Renommer la colonne pour correspondre à votre demande
+    last_close_df = last_close_df.rename(columns={'Close': 'Dernière Close'})
 
-        # Extraire le nombre de jours et créer une nouvelle colonne 'Days'
-        df_symbol['Days'] = (df_symbol['Date'] - df_symbol['Date'].min()) / np.timedelta64(1, 'D')
+    print(last_close_df)
 
-        # Définir X et y
-        X = df_symbol['Days'].values.reshape(-1, 1)
-        y = df_symbol['Close'].values.reshape(-1, 1)
+    # Sélectionner la valeur maximale de 'Close' pour chaque symbole avec la date associée
+    ath_df = df_tri.loc[df_tri.groupby('Symbole')['Close'].idxmax()][['Symbole', 'Date', 'Close']]
+    ath_df = ath_df.rename(columns={'Close': 'ATH'})
+    print(ath_df)
 
-        # Entraîner le modèle
+    # Fusionner les deux DataFrames sur la colonne 'Symbole'
+    result_df = pd.merge(last_close_df, ath_df, on='Symbole')
+
+    # Ajouter une nouvelle colonne avec la dernière date du DataFrame df_tri pour chaque symbole
+    last_date_tri_df = df_tri.groupby('Symbole')['Date'].last().reset_index()
+    last_date_tri_df = last_date_tri_df.rename(columns={'Date': 'Fermeture'})
+    result_df = pd.merge(result_df, last_date_tri_df, on='Symbole')
+
+    # Convertir les colonnes 'Fermeture' et 'Date' en objets datetime
+    result_df['Fermeture'] = pd.to_datetime(result_df['Fermeture'], format='%d/%m/%Y')
+    result_df['Date'] = pd.to_datetime(result_df['Date'], format='%d/%m/%Y')
+
+    # Ajouter une nouvelle colonne avec la différence de jours
+    result_df['Diff_Jours'] = (result_df['Fermeture'] - result_df['Date']).dt.days
+
+    # Définir les conditions
+    recent_condition = (result_df['Diff_Jours'] <= 90)
+    moins_un_an_condition = ((result_df['Diff_Jours'] > 90) & (result_df['Diff_Jours'] <= 365))
+
+    # Appliquer les conditions et créer la nouvelle colonne
+    result_df['Periode'] = 'Plus d\'un an'
+    result_df.loc[recent_condition, 'Periode'] = 'Récent (-3mois)'
+    result_df.loc[moins_un_an_condition, 'Periode'] = 'Moins d\'un an'
+    result_df = result_df.drop(columns=['Fermeture', 'Diff_Jours'])
+    print(result_df)
+
+    # Paramètres
+    start_date = pd.to_datetime('01/01/2024', format='%d/%m/%Y')  # Date de début du modèle
+    start_forecast_date = pd.Timestamp.now()  # Date de début des prédictions
+    days_to_forecast = 30  # Nombre de jours à prédire
+
+    # Convertir la colonne 'Date' en objets de date
+    df_tri['Date'] = pd.to_datetime(df_tri['Date'])
+
+    print(df_tri)
+
+    # Créer une colonne pour stocker les prédictions
+    result_df['Estimation J+30'] = np.nan
+
+    # Pour chaque symbole, entraîner le modèle et prédire les valeurs 30 jours plus tard
+    for symbole in result_df['Symbole']:
+        # Filtrer les données correspondant à chaque symbole
+        symbole_data = df_tri[df_tri['Symbole'] == symbole]
+
+        # Sélectionner les données jusqu'à la date de début du modèle
+        df_train = symbole_data[symbole_data['Date'] >= start_date]
+
+        # Extraire les variables indépendantes (X) et dépendantes (y)
+        X = (df_train['Date'] - start_date) / np.timedelta64(1, 'D')
+        X = X.values.reshape(-1, 1)
+        y = df_train['Close'].values.reshape(-1, 1)
+
+        # Entraîner le modèle de régression linéaire
         regressor = LinearRegression()
         regressor.fit(X, y)
 
-        # Définir la date de début pour les prévisions, en excluant les week-ends
-        start_forecast_date = pd.to_datetime('2024-03-21')
+        # Calculer la pente de la régression linéaire
+        slope = regressor.coef_[0][0]
 
-        # Calculer le nombre de jours ouvrables à prédire
-        days_in_future = 31  # Le nombre total de jours à prédire
-        future_business_days = pd.date_range(start_forecast_date, periods=days_in_future, freq=BDay())
+        # Déterminer la tendance en fonction de la pente
+        trend = "Haussière" if slope > 0 else "Baissière"
 
-        # Convertir les dates futures en nombres, basé sur le calcul original
-        future_forecast_days = [(day - start_date).days for day in future_business_days]
-        future_forecast = np.array(future_forecast_days).reshape(-1, 1)
+        # Ajouter la tendance à la dataframe result_df
+        result_df.loc[result_df['Symbole'] == symbole, 'Tendance'] = trend
 
-        # Prédiction avec le modèle entraîné
-        y_pred = regressor.predict(future_forecast)
+        # Prédire les valeurs 30 jours plus tard
+        future_date = start_forecast_date + pd.DateOffset(days=days_to_forecast)
+        future_value = regressor.predict(np.array((future_date - start_date).days).reshape(1, -1))
 
-        # Calculer la date de l'ATH et la période de l'ATH
-        ath_date = df_symbol.loc[df_symbol['Close'].idxmax(), 'Date']
-        today = datetime.today()
+        # Ajouter la prédiction à result_df
+        result_df.loc[result_df['Symbole'] == symbole, 'Prediction J+30'] = future_value
+        print(result_df)
 
-        if today - ath_date <= timedelta(days=90):
-            ath_period = 'Récent (-3mois)'
-        elif timedelta(days=90) < today - ath_date <= timedelta(days=365):
-            ath_period = 'Moins d\'un an'
-        else:
-            ath_period = 'Plus d\'un an'
+    # Estimation de rendement en pourcentage
+    estimation = ((result_df['Prediction J+30'] - result_df['Dernière Close']) / result_df['Dernière Close']) * 100
+    estimation = estimation.map("{:.2f}%".format)
 
-        # Dernière donnée réelle
-        last_real_data = df_symbol['Close'].iloc[-1]
+    # Ajout de la colonne "Evolution attendue" à result_df
+    result_df['Evolution attendue'] = estimation
+    result_df = result_df.drop(columns=['Estimation J+30'])
 
-        # Dernière donnée prédictive
-        last_predicted_data = y_pred[-1]
-
-        # Déterminer la tendance
-        if last_real_data < last_predicted_data:
-            trend = 'Haussière'
-        else:
-            trend = 'Baissière'
-
-        # Ajouter les prédictions au DataFrame
-        new_row = pd.DataFrame({
-            'Symbole': [symbol],
-            'Dernière Close': [last_real_data],
-            'Date ATH': [ath_date],
-            'ATH': [df_symbol['Close'].max()],
-            'Période ATH': [ath_period],
-            'Tendance': [trend],
-            'Prediction J+30': [y_pred[30]],  # Remplacer par la prédiction J+30
-            'Evolution attendue': [(y_pred[30] - last_real_data) / last_real_data]
-        })
-
-        df_predictions = pd.concat([df_predictions, new_row], ignore_index=True)
-
+    # Renommer la colonne "Date" en "Date ATH"
+    result_df = result_df.rename(columns={'Date': 'Date ATH'})
     # Afficher les prédictions
-    st.dataframe(df_predictions)
+    st.dataframe(result_df)
